80c8fe9657182cb8943ad3aeadd6b125829c192a
Enes <holmstain@gmail.com>
2023-01-30 09:56:02 +0300

    create .env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import './.env'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,13 +10,16 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiklnci8fyCElxZfNVvHokARnSBpY4wI4",
-  authDomain: "holm-to-do.firebaseapp.com",
+  apiKey: "process.env.FIREBASE_API_KEY",
+  authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
   projectId: "holm-to-do",
-  storageBucket: "holm-to-do.appspot.com",
-  messagingSenderId: "608783997993",
-  appId: "1:608783997993:web:9de34641aa4febcecc9038"
+  storageBucket: "process.env.FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.FIREBASE_MESSAGING_SENDER_ID",
+  appId: "process.env.FIREBASE_APP_ID"
 };
+
+
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
