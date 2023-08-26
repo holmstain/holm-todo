3d4ef61bd532c501c8657af7d488cdbf2e622b9d
Enes <holmstain@gmail.com>
2023-08-27 00:52:58 +0300

    dep update & firebase fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import './.env'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase Database
 
 const firebaseConfig = {
   apiKey: "process.env.FIREBASE_API_KEY",
